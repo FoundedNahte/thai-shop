0b535e966a5d0205b66809c5a5d540b660003468
FoundedNahte <56288341+FoundedNahte@users.noreply.github.com>
2022-04-08 01:49:29 -0400

    change

---
+++ backend/src/bin/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+    })
+    .bind(("127.0.0.1, 8080"))?
+    .run()
+    .await
+}
