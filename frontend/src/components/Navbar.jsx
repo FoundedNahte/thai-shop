2603030c53334f7dfafe1d89daffc4f8c8e2e5f6
FoundedNahte <56288341+FoundedNahte@users.noreply.github.com>
2022-04-14 14:35:27 -0400

    navbar change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,23 +3,65 @@ import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import HomeIcon from '@mui/icons-material/Home';
-import Toolbar from '@mui/material/Toolbar'
-const Navbar = () => {
-    return (
-      <Box sx={{ flexGrow: 1}}>
-        <AppBar position="static">
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
+import createTheme from '@mui/material/styles/createTheme';
+import { ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#DED9D9",
+    },
+    secondary: {
+      main: "#000000",
+    },
+  },
+});
+
+const theme2 = createTheme({
+  palette: {
+    primary: {
+      main: "#B6B0B0",
+    },
+    secondary: {
+      main: "#000000",
+    }
+  },
+});
+
+export default function Navbar() {
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar sx={{ height: 50 }}>
             <Toolbar>
-              <IconButton aria-label="home">
-                <HomeIcon />
+              <Typography variant="h6" component="div" gutterBottom={true} sx={{ flexGrow: 1}}>
+                Phone Number : (703) 912-3164 
+              </Typography>
+              <IconButton aria-label="signin" color="inherit" size="large">
+                <AccountCircleIcon />
               </IconButton>
-              <Button variant="contained" startIcon={<ShoppingCartIcon />}>
-                Cart
+              <Button variant="contained" endIcon={<ShoppingCartIcon />} size="large" style={{maxWidth: '50px', maxHeight: "30px", minWidth: "30px", minHeight: "30px", top: "5px"}}>
+                CART
               </Button>
             </Toolbar>
-        </AppBar>
-      </Box>
-    );
-};
-
-export default Navbar;
+          </AppBar>
+        </Box>
+      </ThemeProvider>
+      <ThemeProvider theme={theme2}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar sx={{ height: 105, top: 50 }}>
+            <Toolbar>
+              <Button>
+                THAI SHOP
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </ThemeProvider>
+    </div>
+  );
+}
