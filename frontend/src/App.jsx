40660b5e868a4de56c927d84fd93f01e83b0c93d
Vincent Song <dela.vincent.15@gmail.com>
2022-06-24 13:13:58 -0400

    Added root path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+         <Route path="/" element = {<Home/>}/>
          <Route path="/home" element = {<Home/>}/>
          <Route path="/shop" element = {<Shop/>}/>
          <Route path="/signin" element = {<SignIn/>}/>
