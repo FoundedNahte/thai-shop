0a3957404f42088e2633e4f5465238327f5e7cee
FoundedNahte <56288341+FoundedNahte@users.noreply.github.com>
2022-07-07 17:41:28 -0400

    old change

--- new_backend/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod configuration;
-pub mod populate;
-pub mod routes;
-pub mod startup;
-pub mod telemetry;
-pub mod domain;
--- new_backend/src/routes/mod.rs
+++
@@ -1,7 +0,0 @@
-mod fetch;
-mod health_check;
-mod react_page;
-
-pub use fetch::*;
-pub use health_check::*;
-pub use react_page::*;
