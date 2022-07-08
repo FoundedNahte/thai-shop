f5d4f0b6c9495e6fb5c2871411e8687cdfa9e9ef
Ethan <eclau2003@gmail.com>
2022-07-08 16:47:52 -0400

    remove

--- backend_test/migrations/20220531155533_create_item_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE items(
-	name TEXT NOT NULL,
-	id uuid NOT NULL,
-	category TEXT NOT NULL,
-	description TEXT NOT NULL,
-	price float NOT NULL,
-	discount float NOT NULL
-)
--- backend_test/src/domain/mod.rs
+++
@@ -1,3 +0,0 @@
-mod item;
-
-pub use item::*;
